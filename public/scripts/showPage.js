33be343c04ae0d560d77f10a06917b017f92970b
AbhinavSivanandhan <abhinavstar.anand@gmail.com>
2023-06-21 18:51:41 +0530

    clustermap fixed-prototype done

--- public/scripts/showPage.js
+++ public/scripts/showPage.js
@@ -143,7 +143,16 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
 container: 'map', // container ID
 style: 'mapbox://styles/mapbox/streets-v12', // style URL
-center: [-74.5, 40], // starting position [lng, lat]
+center: occasion.geometry.coordinates, // starting position [lng, lat]
 zoom: 9, // starting zoom
 });
 
+new mapboxgl.Marker()
+    .setLngLat(occasion.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 })
+            .setHTML(
+                `<h3>${occasion.title}</h3><p>${occasion.location}</p>`
+            )
+    )
+    .addTo(map)
